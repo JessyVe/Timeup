072cfc6c37414db67a6bd8439a274fe73bc6457a
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-05-26 11:44:36 +0200

    Implement authentification

--- Timeup/app/src/main/java/com/fh/android/timeup/RegisterActivity.kt
+++ Timeup/app/src/main/java/com/fh/android/timeup/RegisterActivity.kt
@@ -2,7 +2,10 @@ package com.fh.android.timeup
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.register_screen.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -12,9 +15,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.register_screen)
 
         btRegister.setOnClickListener{
-            val email = txEmail.text.toString()
-            val username = txUsername.text.toString()
-            val password = txPassword.text.toString()
+            performRegistration()
         }
 
         tvShowLogin.setOnClickListener {
@@ -22,5 +23,32 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegistration(){
+        val email = txEmail.text.toString()
+        val username = txUsername.text.toString()
+        val password = txPassword.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Email and password may not be empty!", Toast.LENGTH_SHORT).show()
+            return
+        } else if(password.length < 6){
+            Toast.makeText(this, "Password most consist of at least 6 characters", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Firebase auth
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful)return@addOnCompleteListener
+
+                Log.d("Register", "Successfully crated user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener{
+                val errorText = "Failed to create user! ${it.message}"
+                Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show()
+                Log.d("Register", errorText)
+            }
     }
 }
