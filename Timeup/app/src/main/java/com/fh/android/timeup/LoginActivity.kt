7d04f043e51c9ce4f844fb2f1f0148d0f27a2cf4
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-05-26 13:03:10 +0200

    Make control corners rounded

--- Timeup/app/src/main/java/com/fh/android/timeup/LoginActivity.kt
+++ Timeup/app/src/main/java/com/fh/android/timeup/LoginActivity.kt
@@ -2,7 +2,10 @@ package com.fh.android.timeup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.txEmail
+import kotlinx.android.synthetic.main.activity_login.txPassword
 
 class LoginActivity : AppCompatActivity(){
 
@@ -10,6 +13,19 @@ class LoginActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        btLogin.setOnClickListener {
+            val email = txEmail.text.toString()
+            val password = txPassword.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+
+                }
+                .addOnFailureListener{
+
+                }
+        }
 
         tvShowRegistration.setOnClickListener {
             finish()
